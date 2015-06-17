e4300459208a8480b838f20bb2521b93aaca1c0b
unknown <SBT-Vybornov-VV@DF-070415-RN10.sigma.sbrf.ru>
2015-06-17 16:49:43 +0300

    Added jade

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ var LISTEN_PORT = 3000;
 
 var app = express();
 
+var jade = require('jade');
+var main = jade.compileFile("./templates/main.jade", {});
+
+//// Render the function
+//var html = fn(locals);
+//// => '<string>of jade</string>'
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world ФФФФ');
+    res.set('Content-Type', 'text/html');
+    res.send(main({}));
 });
 
 app.listen(LISTEN_PORT, function() {
